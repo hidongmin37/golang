ebbf65e22a05d8bb8d8639da7cdf9cfe52046964
hidongmin37 <jangdm37@gmail.com>
2026-01-30 10:33:52 +0900

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//	func main() {
+//		fmt.Println("Hello World")
+//		something.SayHello()
+//		something.SayBye()
+//
+// }
+func main() {
+	name := true
+	name = "min"
+	//const name string = "dongmin"
+	fmt.Println(name)
+}
